cc0e0601c810b0e630177c8ecec00542c3b93db5
Hyunsung Cho <hscho1226@gmail.com>
2021-02-12 13:33:48 +0900

    Add MobileHCI reviewer

--- src/NewsData.js
+++ src/NewsData.js
@@ -4,34 +4,38 @@
 let newsData = {
   "news": [
     {
+      "date": "May 2021",
+      "text": "👩🏻‍💻 Starting my internship at Nokia Bell Labs, Cambridge, UK."
+    },
+    {
       "date": "Jan 2021",
-      "text": "👩🏻‍💻 Starting my internship at Nokia Bell Labs, Cambridge, UK"
+      "text": "👩🏻‍🏫 Presentation at HCI Korea 2021 Positive Computing Workshop."
     },
     {
       "date": "Dec 2020",
-      "text": "👩🏻‍🏫 Virtually attending CSCW Asia Winter School on Dec 18-19"
+      "text": "👩🏻‍🏫 SIGCHI CSCW Asia Winter School (AWS) 2020 on Dec 18-19."
     },
     {
       "date": "Nov 2020",
-      "text": "🏆 Won the NAVER Ph.D. Fellowship Award"
+      "text": "🏆 NAVER Ph.D. Fellowship Award."
     },
     {
       "date": "Oct 2020",
-      "text": "👩🏻‍🏫 Virtually attending CSCW 2020 for my paper presentation and demo"
+      "text": "👩🏻‍🏫 Paper presentation & demo at CSCW 2020."
 
     },
     {
       "date": "Aug 2020",
-      "text": "👩🏻‍🏫 Presenting my ongoing work on feature-level analysis of regretful social media use at HCI@KAIST " +
-          "Summer Workshop",
+      "text": "👩🏻‍🏫 Presentation at 2020 HCI@KAIST " +
+          "Summer Workshop.",
     },
     {
       "date": "Mar 2020",
-      "text": "🗞 Paper accepted to ACM CSCW 2020"
+      "text": "🗞 Paper accepted to ACM CSCW 2020."
     },
     {
       "date": "Feb 2020",
-      "text": "👩🏻‍🎓 Earned an MS degree in Computer Science"
+      "text": "👩🏻‍🎓 Officially a Master in Computer Science."
     },
     // {
     //   "date": "Feb 2020",
@@ -39,24 +43,23 @@ let newsData = {
     // },
     {
       "date": "Sep 2019",
-      "text": "🏆 Best TA Award of KAIST CS for _CS341: Intro to Networking_"
+      "text": "🏆 Best TA Award of KAIST CS for _CS341: Intro to Networking_."
     },
     {
       "date": "Aug 2019",
-      "text": "✈️ Attending the Google WTM Scholars Retreat (Aug 27-Sep 1) in Sydney, Australia",
+      "text": "✈️ Google WTM Scholars Retreat (Aug 27-Sep 1) in Sydney, Australia.",
     },
     {
       "date": "Aug 2019",
-      "text": "👩🏻‍🏫 Presenting my ongoing work on sender-controlled mobile instant message notifications at HCI@KAIST " +
-          "Summer Workshop",
+      "text": "👩🏻‍🏫 Presentation at 2019 HCI@KAIST Summer Workshop.",
     },
     {
       "date": "Jul 2019",
-      "text": "🏆 Selected as a scholarship recipient of the 2019 Google Women Techmakers (WTM) Scholars Program"
+      "text": "🏆 Google Women Techmakers (WTM) Scholar 2019."
     },
     {
       "date": "Jul 2019",
-      "text": "🗞 Paper accepted to ACM IMWUT (UbiComp) 2019"
+      "text": "🗞 Paper accepted to ACM IMWUT (UbiComp) 2019."
     }
   ],
 };
