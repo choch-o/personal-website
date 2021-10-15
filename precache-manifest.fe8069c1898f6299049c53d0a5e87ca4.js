1948d34d07b009090fdbce5a6f3a22ddcf618176
Hyunsung Cho <hyunsung@cs.cmu.edu>
2021-10-15 17:31:26 -0400

    Updates

---
+++ precache-manifest.fe8069c1898f6299049c53d0a5e87ca4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "221e0b362c54ac41bdc7",
+    "url": "/static/js/main.221e0b36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "05840be1f031421e00f2",
+    "url": "/static/js/2.05840be1.chunk.js"
+  },
+  {
+    "revision": "a39fe4e837c0362d0efa298a2820802f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.be381b14da3d5bd7ea3224eafc574a46.js"
+  "/precache-manifest.fe8069c1898f6299049c53d0a5e87ca4.js"
 );
 
 workbox.clientsClaim();
